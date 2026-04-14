62e902d81a663dfd065570aefad7dec02c929220
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-04-14 13:34:01 +0200

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length < 2) return strs[0];
+        Arrays.sort(strs);
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            if (strs[0].charAt(0) != strs[1].charAt(0)) return "";
+            else if (strs[0].charAt(i) != strs[1].charAt(i)) break;
+            s.append(strs[0].charAt(i));
+        }
+        for (int i = 2; i < strs.length; i++) {
+            if (strs[i].contains(s.toString())) continue;
+            else s.deleteCharAt(s.length()-1);
+        }
+        return s.toString();
+    }
+}
