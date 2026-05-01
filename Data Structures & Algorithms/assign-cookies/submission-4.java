1c43e2ec422fee3530c795dcac24373badada87b
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-05-01 18:37:25 +0200

    Add: assign-cookies - submission-4

---
+++ Data Structures & Algorithms/assign-cookies/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0, j = 0;
+        Arrays.sort(s);
+        Arrays.sort(g);
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+        return i;
+    }
+}
