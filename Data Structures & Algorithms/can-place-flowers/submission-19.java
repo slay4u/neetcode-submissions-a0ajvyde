002ebf07ca7ff5cb306e537b5e58ad64959dfc02
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-05-03 15:55:59 +0200

    Add: can-place-flowers - submission-19

---
+++ Data Structures & Algorithms/can-place-flowers/submission-19.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int i = 0;
+        while (i < flowerbed.length) {
+            if ((flowerbed.length == 1 && flowerbed[0] == 0) 
+            || (i == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) 
+            || (i == flowerbed.length-1 && flowerbed[i] == 0 && flowerbed[i-1] == 0)
+            || (i != 0 && flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) return true;
+            } 
+            i++;
+        }
+        return n <= 0;
+    }
+}
